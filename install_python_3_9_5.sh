04f991a0ab18d80fa48a617a8fc7c76a83771442
sylents-dev-e <dev.e@sylents.de>
2021-06-23 15:47:13 +0200

    re-added .sh file for raspi python.

---
+++ install_python_3_9_5.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# @jwa
+
+sudo apt update
+
+# install essential packages for compiling source code.
+sudo apt install wget build-essential checkinstall
+sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+
+# Download the Python 3.9.5 source code from the official download site
+wget https://www.python.org/ftp/python/3.9.5/Python-3.9.5.tar.xz
+
+#unpack the file
+tar -Jxf Python-3.9.5.tar.xz
+
+cd Python-3.9.5
+./configure --enable-optimizations
+
+# compile the source, it?s a good time to get a coffee and a good book.
+make
+
+# replaces your previous Python installation!!
+sudo make install
+
+cd ..
+sudo rm -r Python-3.9.5
+rm -rf Python-3.9.5.tar.xz
+
+Python --version
+
+
+echo "alias python='/usr/local/bin/python3.9'" >> ~/.bashrc
+echo "alias pip=pip3" >> ~/.bashrc
+
+. ~/.bashrc

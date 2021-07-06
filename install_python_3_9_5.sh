4de002de3a947684f840141108f3c06aa3f6d94f
hans sylents <hans@sylents.de>
2021-07-06 16:20:34 +0200

    lzma lib added

--- install_python_3_9_5.sh
+++ install_python_3_9_5.sh
@@ -6,7 +6,7 @@ sudo apt update
 
 # install essential packages for compiling source code.
 sudo apt install wget build-essential checkinstall
-sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev s-dev
 
 
 # Download the Python 3.9.5 source code from the official download site
